75e24223087f75c89bd7b01729c1d068102af47d
Kelvin Omereshone <kelvinomereshone@gmail.com>
2020-08-15 19:53:26 +0100

    feat: more sails lift commands

--- src/util.ts
+++ src/util.ts
@@ -1,26 +1,26 @@
 import * as vscode from "vscode";
-import * as fs from 'fs';
+import * as fs from "fs";
 
 export const getFilePath = (text: string, document: vscode.TextDocument) => {
-    const controllersPath = '/api/controllers'
-    let filePath = vscode.workspace.getWorkspaceFolder(document.uri)?.uri.fsPath + controllersPath;
-    let splitted = text.replace(/'|"/g, "").split("/");
-    let controllerFileName = splitted[0];
-    let actionFileName = splitted[1] + ".js";
-    console.log(text);
-    let targetPath = filePath + "/" + controllerFileName + '/' + actionFileName;
-    console.log(targetPath);
+  const controllersPath = "/api/controllers";
+  let filePath =
+    vscode.workspace.getWorkspaceFolder(document.uri)?.uri.fsPath +
+    controllersPath;
+  let splitted = text.replace(/'|"/g, "").split("/");
+  let controllerFileName = splitted[0];
+  let actionFileName = splitted[1] + ".js";
+  let targetPath = filePath + "/" + controllerFileName + "/" + actionFileName;
+  if (fs.existsSync(targetPath)) {
+    return targetPath;
+  }
+
+  let dirItems = fs.readdirSync(filePath);
+
+  for (let item of dirItems) {
+    targetPath = filePath + "/" + item + "/" + actionFileName;
     if (fs.existsSync(targetPath)) {
-        return targetPath
+      return targetPath;
     }
-
-    let dirItems = fs.readdirSync(filePath);
-
-    for (let item of dirItems) {
-        targetPath = filePath + '/' + item + '/' + actionFileName;
-        if (fs.existsSync(targetPath)) {
-            return targetPath;
-        }
-    }
-    return null;
-}
+  }
+  return null;
+};
