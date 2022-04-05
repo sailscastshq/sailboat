f8d233dedffea7d57fc63649965069b778c22d09
Adam Galloway <adam@launchcode.xyz>
2022-04-05 04:26:56 +0100

    TypeScript support for actions

--- src/extension.ts
+++ src/extension.ts
@@ -25,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
             { text: liftCommand.text }
           );
         } catch (error) {
-          vscode.window.showErrorMessage(error);
+          // vscode.window.showErrorMessage(error);
         }
       }
     );
--- src/util.ts
+++ src/util.ts
@@ -8,10 +8,18 @@ export const getFilePath = (text: string, document: vscode.TextDocument) => {
     controllersPath;
   let splitted = text.replace(/'|"/g, "").split("/");
   let controllerFileName = splitted[0];
+
   let actionFileName = splitted[1] + ".js";
   let targetPath = filePath + "/" + controllerFileName + "/" + actionFileName;
   if (fs.existsSync(targetPath)) {
     return targetPath;
+  }
+
+  let actionFileNameTs = splitted[1] + ".ts";
+  let targetPathTs = filePath + "/" + controllerFileName + "/" + actionFileNameTs;
+
+  if(fs.existsSync(targetPathTs)) {
+    return targetPathTs;
   }
 
   let dirItems = fs.readdirSync(filePath);
